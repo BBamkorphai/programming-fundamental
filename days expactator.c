1631ae3ccc7cbb729cedbd7b51b6d8028db223d9
BBamkorphai <korphaisk@gmail.com>
2022-08-18 14:45:29 +0700

    message

---
+++ days expactator.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main()
+{
+	int day, month, daycount = 0, i, ans;
+	scanf("%d %d", &day, &month);
+	daycount = daycount + day;
+	if( month>1 )
+	{
+        for ( i=1 ; i<month ; i++ )
+		{
+            if(i==1 || i==3 || i==5 || i==7 || i==8 || i==10 || i==12)
+			{
+                daycount += 31;
+            }
+			else if ( i==4 || i==6 || i==9 || i==11 )
+			{
+                daycount += 30;
+            }else if ( i==2 )
+			{
+                daycount += 28;
+            }
+        }
+    }
+	else if ( month=1 )
+	{
+        daycount += 0;
+    }
+	ans = daycount%7;
+	switch(ans)
+	{
+		case 0:
+			printf("Wednesday");
+			break;
+		case 1:
+			printf("Thursday");
+			break;
+		case 2:
+			printf("Friday");
+			break;
+		case 3:
+			printf("Saturday");
+			break;
+		case 4:
+			printf("Sunday");
+			break;
+		case 5:
+			printf("Monday");
+			break;
+		case 6:
+			printf("Tuseday");
+			break;
+	}
+	return 0;
+	
+}
---
+++ days expactator2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int day,month,countday,ans,i;
+    char Day[][7] = {"Wednes","Thurs","Fri","Satur","Sun","Mon","Tues"};
+    scanf("%d %d", &day, &month);
+    countday = day;
+    if(month>1)
+	{
+        for(i=1; i<month; i++)
+		{
+            if(i==1 || i==3 || i==5 || i==7 || i==8 || i==10 || i==12)
+			{
+                countday += 31;
+            }
+			else if(i==4 || i==6 || i==9 || i==11)
+			{
+                countday += 30;
+            }
+			else if(i==2)
+			{
+                countday += 28;
+            }
+        }
+    }
+	else if(month=1)
+	{
+        countday += 0;
+    }
+    ans = countday%7;
+    printf("%sday", Day[ans]);
+}
